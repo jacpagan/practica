19bbf4e41f3fd09b020ae0f110f4b27a3440a0d2
jacpagan <jacpagan@users.noreply.github.com>
2025-08-25 08:50:58 -0700

    Simplify URLs to basic Django welcome page for testing

--- practika_project/urls.py
+++ practika_project/urls.py
@@ -11,28 +11,17 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import RedirectView
+from django.urls import path
+from django.http import HttpResponse
+
+def simple_home(request):
+    return HttpResponse("Django app is running!")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("exercises.urls")),  # Main frontend and API routes
-    path("api/", include("comments.urls")),
-    path("api/", include("core.urls")),
+    path('admin/', admin.site.urls),
+    path('', simple_home, name='home'),
 ]
-
-# Add media files serving in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Add health check endpoint at root level for load balancers
-if getattr(settings, 'HEALTH_CHECK', {}).get('ENABLED', True):
-    from core.views import health_check
-    urlpatterns.insert(0, path("health/", health_check, name="root_health_check"))
