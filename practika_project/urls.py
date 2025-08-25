ce5cbcd92ee27e3c81c60f4f40b52dbd8749cdde
jacpagan <jacpagan@users.noreply.github.com>
2025-08-25 11:33:00 -0700

    Force new build

--- practika_project/urls.py
+++ practika_project/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def simple_home(request):
-    return HttpResponse("Django app is running successfully!", content_type="text/plain")
+    return HttpResponse("Django app is running successfully! Test 2", content_type="text/plain")
 
 urlpatterns = [
     path('', simple_home, name='home'),
